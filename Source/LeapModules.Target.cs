ca97b430f5f1457f3a7883d322498a21df0da1d4
Max Palmer <max.palmer@ultraleap.com>
2020-12-03 17:16:51 +0000

    UNREAL-73 Update copyright notices

--- Source/LeapModules.Target.cs
+++ Source/LeapModules.Target.cs
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 1998-2020 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
